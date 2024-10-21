9aa4c27554f526902edf269b47f287712e01e8ca
Pranjal Gupta <pgupta32@apple.com>
2024-10-21 15:17:18 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+/**
+ * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
+ */
+(function ($) {
+    "use strict";
+      $('.sakura-falling').sakura();
+})(jQuery);
+
+/**
+ *
+ * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
+ *
+ * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
+ * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
+ * Let's keep the sky-high status of this song untouched!
+ *
+ * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
+ * Choose a heart-touching track! 🎵 ❤️
+ *
+ * Listen here: https://youtu.be/X0MDALpV29s
+ *
+ */
+$(document).on('click', function(){
+    document.getElementById("my_audio").play();
+    console.log('Shaadi me zaroor aana');
+});
+
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 25, 2024 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+    }
+}, 1000);
+
+$(document).ready(function () {
+$('#rsvp-form').on('submit', function (e) {
+    e.preventDefault();
+    var data = $(this).serialize();
+    data += "&url=" + encodeURIComponent(window.location.href);
+
+    $('#alert-wrapper').html(alert_markup('info', 'Saving your details...'));
+
+    $.post('https://script.google.com/macros/s/AKfycbwKCdL9Eun7Z4mezfNUEFba9FpwY4eYi9IxEKb7cQqRxGoZvFiWwcxPk3QgEKdlozef/exec', data)
+            .done(function (data) {
+                console.log(data);
+                if (data.result === "error") {
+                    $('#alert-wrapper').html(alert_markup('danger', data.message));
+                } else {
+                    $('#alert-wrapper').html(alert_markup('info', 'Thank you for joining us!'));
+                }
+            })
+            .fail(function (data) {
+                console.log(data);
+                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+            });
+});
+});
+
+
+// alert_markup
+function alert_markup(alert_type, msg) {
+    return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '</div>';
+}
