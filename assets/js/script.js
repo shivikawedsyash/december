112202b8a3b26f5baffb6699569f23a92963a730
Yash Tyagi <yash.tyagi@salesforce.com>
2024-10-22 11:43:55 +0530

    As the group's self-proclaimed best dev, I gracefully accept the burden of improving this masterpiece. You’re welcome. 😎🎨

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,98 @@
-/**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
- */
 (function ($) {
     "use strict";
-      $('.sakura-falling').sakura();
+    $('.sakura-falling').sakura();
 })(jQuery);
 
 /**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
- * Choose a heart-touching track! 🎵 ❤️
- *
- * Listen here: https://youtu.be/X0MDALpV29s
- *
+ * Play audio when the page is clicked
  */
-$(document).on('click', function(){
+$(document).on('click', function () {
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
 });
 
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 5, 2024 00:00:00").getTime();
+// Dates for the countdowns for each event
+var countdownDates = {
+    'engagement': new Date("Dec 2, 2024 00:00:00").getTime(),
+    'wedding': new Date("Dec 6, 2024 00:00:00").getTime()
+};
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// Initialize countdown with the engagement date as default
+var countDownDate = countdownDates['engagement'];
 
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
-    }
-}, 1000);
+// Function to start the countdown
+function startCountdown() {
+    clearInterval(x); // Clear any previous countdown interval
+    x = setInterval(function () {
 
-$(document).ready(function () {
-$('#rsvp-form').on('submit', function (e) {
-    e.preventDefault();
-    var data = $(this).serialize();
-    data += "&url=" + encodeURIComponent(window.location.href);
+        // Get today's date and time
+        var now = new Date().getTime();
 
-    $('#alert-wrapper').html(alert_markup('info', 'Saving your details...'));
+        // Find the distance between now and the countdown date
+        var distance = countDownDate - now;
 
-    $.post('https://script.google.com/macros/s/AKfycbwKCdL9Eun7Z4mezfNUEFba9FpwY4eYi9IxEKb7cQqRxGoZvFiWwcxPk3QgEKdlozef/exec', data)
-            .done(function (data) {
-                console.log(data);
-                if (data.result === "error") {
-                    $('#alert-wrapper').html(alert_markup('danger', data.message));
-                } else {
-                    $('#alert-wrapper').html(alert_markup('info', 'Thank you for joining us!'));
-                }
-            })
-            .fail(function (data) {
-                console.log(data);
-                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-            });
-});
-});
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+        // Output the result in an element with id="time"
+        document.getElementById("time").innerHTML =
+            "<div class='container'>" +
+            "<div class='days block'>" + days + "<br>Days</div>" +
+            "<div class='hours block'>" + hours + "<br>Hours</div>" +
+            "<div class='minutes block'>" + minutes + "<br>Minutes</div>" +
+            "<div class='seconds block'>" + seconds + "<br>Seconds</div>" +
+            "</div>";
 
-// alert_markup
-function alert_markup(alert_type, msg) {
-    return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '</div>';
+        // If the countdown is over, display a message
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("time").innerHTML = "Bless the married couple for a happy life!";
+        }
+    }, 1000);
 }
+
+// Call countdown initially for engagement
+var x; // Define x globally for clearInterval
+startCountdown();
+
+// Function to handle tab switching
+// Function to switch between tabs and play the correct audio
+function openTab(event, tabName) {
+    // Hide all tab contents
+    var tabContents = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove("active");
+    }
+
+    // Remove the active class from all tabs
+    var tabLinks = document.getElementsByClassName("tab-link");
+    for (var i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active");
+    }
+
+    // Show the current tab and set it as active
+    document.getElementById(tabName).classList.add("active");
+    event.currentTarget.classList.add("active");
+
+    // Stop all audio
+    document.getElementById('engagement-audio').pause();
+    document.getElementById('engagement-audio').currentTime = 0;
+    document.getElementById('wedding-audio').pause();
+    document.getElementById('wedding-audio').currentTime = 0;
+
+    // Play the correct audio for the selected tab
+    if (tabName === 'engagement') {
+        document.getElementById('engagement-audio').play();
+    } else if (tabName === 'wedding') {
+        document.getElementById('wedding-audio').play();
+    }
+}
+
+// Automatically play the engagement song on page load
+window.onload = function() {
+    document.getElementById('engagement-audio').play();
+}
+
